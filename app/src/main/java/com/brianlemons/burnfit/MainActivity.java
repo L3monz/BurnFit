429842d770737aa6260202b338c4b9854d761913
L3monz <brianlemons2013@gmail.com>
2017-03-18 14:42:58 -0400

    Added Tabs

--- app/src/main/java/com/brianlemons/burnfit/MainActivity.java
+++ app/src/main/java/com/brianlemons/burnfit/MainActivity.java
@@ -1,13 +1,27 @@
 package com.brianlemons.burnfit;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private TextView infoTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        infoTextView = (TextView) findViewById(R.id.infoTextView);
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+
     }
 }
