b609ca779b2192e234cf105d5524e4e6b4da86d3
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-04 11:39:14 -0400

    removed a line of whitespace in strings.xml

---
+++ app/src/main/java/com/brianlemons/burnfit/FoodActivity.java
@@ -0,0 +1,11 @@
+package com.brianlemons.burnfit;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ashwin on 4/4/2017.
+ */
+
+public class FoodActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/brianlemons/burnfit/FoodFragment.java
@@ -0,0 +1,12 @@
+package com.brianlemons.burnfit;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Ashwin on 4/4/2017.
+ */
+
+public class FoodFragment extends Fragment{
+    //TODO get the activity here and redirect to the page
+    // TODO with the suggestions and recipe
+}
