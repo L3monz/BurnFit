4cda68a989dec54c0fb1ece06b06007cb349dd59
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-05 16:30:12 -0400

    more changes to food

--- app/src/main/java/com/brianlemons/burnfit/FoodActivity.java
+++ app/src/main/java/com/brianlemons/burnfit/FoodActivity.java
@@ -1,8 +1,12 @@
 package com.brianlemons.burnfit;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,14 +22,24 @@ public class FoodActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_suggestions);
 
-        /*Button nextPageButton = (Button) findViewById(R.id.nextPageFood);
-        nextPageButton.setOnClickListener(new View.OnClickListener(){
+        /*BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                //to fix
-                //Intent i = new Intent(getApplicationContext(), FoodActivity.class);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.exerciseItem) {
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                    ((Activity) getApplicationContext()).overridePendingTransition(0,0);
+                } else if (item.getItemId() == R.id.foodItem) {
+                } else if (item.getItemId() == R.id.exploreItem) {
+                } else if (item.getItemId() == R.id.funItem) {
+                }
+                return false;
             }
         });*/
+
     }
 
 }
