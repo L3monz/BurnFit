fc6e84ff9a55e04e3cc54ba483c4dcbaf7f4f83b
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-04 22:08:27 -0400

    fixed picture for the food suggestion

--- app/src/main/java/com/brianlemons/burnfit/FoodActivity.java
+++ app/src/main/java/com/brianlemons/burnfit/FoodActivity.java
@@ -14,7 +14,7 @@ public class FoodActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_food_suggestions);
+        //setContentView(R.layout.activity_food_suggestions);
     }
 
 }
