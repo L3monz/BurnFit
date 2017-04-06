ed6ad30b1e9b3c4a39dbec5388d3559e1dfb84fc
ltesfay1 <lydia.tesfaye@yahoo.com>
2017-04-06 05:37:11 -0400

    java files
    just added java files, about to work on layouts some more

---
+++ app/src/main/java/com/brianlemons/burnfit/ExploreEatOutRight.java
@@ -0,0 +1,62 @@
+package com.brianlemons.burnfit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import static com.brianlemons.burnfit.FoodActivity.removeShiftMode;
+
+/**
+ * Created by lidya on 4/6/2017.
+ */
+
+public class ExploreEatOutRight extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    public ListView listViewEvents;
+    public TextView eat, events;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_events);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById
+                (R.id.bottomNavigationView);
+        removeShiftMode(bottomNavigationView);
+        bottomNavigationView.getMenu().getItem(0).setChecked(false);
+        bottomNavigationView.getMenu().getItem(1).setChecked(false);
+        bottomNavigationView.getMenu().getItem(2).setChecked(false);
+        bottomNavigationView.getMenu().getItem(3).setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.exerciseItem) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(0, 0);
+                } else if (item.getItemId() == R.id.foodItem) {
+                    //start the food activity
+                    Intent i = new Intent(getApplicationContext(), FoodManager.class);
+                    startActivity(i);
+                    overridePendingTransition(0, 0);
+
+                } else if (item.getItemId() == R.id.exploreItem) {
+                } else if (item.getItemId() == R.id.funItem) {
+                    Intent intent = new Intent(getApplicationContext(), Events.class);
+                    startActivity(intent);
+                    overridePendingTransition(0, 0);
+                }
+                return false;
+            }
+        });
+    }
+}
