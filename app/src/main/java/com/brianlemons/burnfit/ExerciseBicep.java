e8b5f6ec1afab1685a63abaae3182ea9a9b1acff
L3monz <brianlemons2013@gmail.com>
2017-04-06 00:28:25 -0400

    Fixed food activity

--- app/src/main/java/com/brianlemons/burnfit/ExerciseBicep.java
+++ app/src/main/java/com/brianlemons/burnfit/ExerciseBicep.java
@@ -1,13 +1,22 @@
 package com.brianlemons.burnfit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.youtube.player.YouTubeIntents;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by brianlemons on 3/25/17.
@@ -28,6 +37,34 @@ public class ExerciseBicep extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_recommendations);
 
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        /* Use this to switch between tabs. */
+        removeShiftMode(bottomNavigationView);
+        bottomNavigationView.getMenu().getItem(0).setChecked(true);
+        bottomNavigationView.getMenu().getItem(1).setChecked(false);
+        bottomNavigationView.getMenu().getItem(2).setChecked(false);
+        bottomNavigationView.getMenu().getItem(3).setChecked(false);
+
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.exerciseItem) {
+                } else if (item.getItemId() == R.id.foodItem) {
+                    //start the food activity
+                    Intent i = new Intent(getApplicationContext(), FoodManager.class);
+                    startActivity(i);
+                    overridePendingTransition(0,0);
+
+                } else if (item.getItemId() == R.id.exploreItem) {
+                } else if (item.getItemId() == R.id.funItem) {
+                }
+                return false;
+            }
+        });
+
         ExerciseVideoAdapter exerciseListAdapter = new ExerciseVideoAdapter(this,exerciseDescription,exerciseThumbnail);
         exerciseLv = (ListView) findViewById(R.id.exerciseVideos);
         workOut = (TextView) findViewById(R.id.workout);
@@ -43,5 +80,24 @@ public class ExerciseBicep extends AppCompatActivity {
             }
         });
 
+    }
+    static void removeShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("ERROR NO SUCH FIELD", "Unable to get shift mode field");
+        } catch (IllegalAccessException e) {
+            Log.e("ERROR ILLEGAL ALG", "Unable to change value of shift mode");
+        }
     }
 }
